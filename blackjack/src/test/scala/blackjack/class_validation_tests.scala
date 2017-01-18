faccaec91f3d0d3a87f213512a8646283b9f4200
shutchison <scott.hutchison@usma.edu>
2017-01-18 00:01:16 -0500

    Deleted extra test harness

--- blackjack/src/test/scala/blackjack/class_validation_tests.scala
+++
@@ -1,9 +0,0 @@
-package blackjack
-
-import org.scalatest.FunSpec
-import org.scalatest.Matchers
-import scala.collection.mutable.ArrayBuffer
-
-class class_validation_tests extends FunSpec with Matchers {
-    
-}
